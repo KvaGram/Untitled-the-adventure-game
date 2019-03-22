77288d3fa73c601249b06e0d7eed51f9ab74e057
Lars Erik Grambo <0kvakk@gmail.com>
2019-03-22 14:16:15 +0100

    More story on the reactor scene

--- outer.py
+++ outer.py
@@ -1,4 +1,5 @@
 import game_utilities as game
+import random
 
 class roomdata:
     ind = 0 #current location, index
@@ -114,6 +115,7 @@ After that, aside from what looks to be some solid tick tube, there is nothing b
 darkness and... lights. moving lights.
 Stars, you realize. Stars flying upwards. You are staring into space!
     """, 0.3)
+
     def reactNode():
         game.showtext("Placeholder. Yeah, important path in the game here. Sorry it is incomplete!")
         game.rolltext("""
@@ -132,8 +134,8 @@ Reactor-ring control node, radiation hazard, chemical hazard, tachyon hazard.
         game.rolltext("""
 Alarms bleeping, lights flashing, plasma leaking, then unleaking, as small pockets of local spacetime loops.
 As you enter the room you instinctivly step aside to let a oddly familiar {0} out of the room.
-Suprisingly, {1} looked oddly familiar. The {0} looked as shocked as you did.
-But as suddenly as {1} appeared, {1} suddenly dissappeard.
+Suprisingly, {1} looked oddly familiar. Looking up from {1} hands and at you, the {0} looks as shocked as you.
+But as suddenly as {1} appeared, the {0} suddenly dissappeard.
 
 What was {1}? A ghost?
 
@@ -146,8 +148,9 @@ it's gone.
 no, now it's there again.
 ..and it's gone!
         """)
-        c = game.choose2(("try to snatch the manual next time it appears", "randomly enter data into the control-screen"), "What will you do?")
-        if(c == 1):
+        ind, val = game.choose2(("try to snatch the manual next time it appears", "randomly enter data into the control-screen"), "What will you do?")
+        #stupid choice resulting in a game over:
+        if(ind == 1):
             game.setGameover(save, "You got yourself trapped in a timeloop")
             game.rolltext("""
 Not knowing what to do, you start hammering on the controls.
@@ -172,6 +175,188 @@ And you soon realied why.
 Aside from your little looping bobble, everything was gone.
 You aren't dead. Not really. But by now, you wish you were.
             """)
+            nav.running = False
+            return
+        #end of game over stupity
+        game.rolltext("""
+You hover your hand over where the manual were a secund ago.
+...
+There it is! you snatch it!
+..!
+Ouch!
+You got the manual book in your hand, but your fingers are now covered in a layor of some oily substance.
+And they feel cold, really cold and numb.
+Looking at the where you picked up the manual, it suddenly flashed in again, despite also being in your hand.
+Moreover, there was now something else.. your fingers!
+your disenbodied fingers appearing out of nowhere to grab the manual,
+only to dissappear, and reappear.
+You decide to ignore this odd phenomenon, as you focus on your task.
+
+Browsing the manual you find instructions for a number of emergency procedures.
+        """)
+        #You need to first initalize the emergency cooling system, then the emergency particle decelerator.
+        choices = [
+            ("COOL DOWN", "Initiate Emergency cooling system"),
+            ("SLOW DOWN", "Engage emergency particle decelerator"),
+            ("HURRY UP", "Engage emergency particle accelerator"),
+            ("FIRE SUP 1", "Engage accellerator fire suppression system"),
+            ("FIRE SUP 2", "Engage control room fire suppression system"),
+            ("INFO", "Run automatic diagnostics"),
+            ("INSTADEATH", "Engage emergency overload"),
+            ("REVERSE", "Depolorize the tacheon emitter"),
+            ("SHUTDOWN", "Shutdown tacheon emitter array")
+            ]
+        random.shuffle(choices)
+        cooledDown = False # must be done first
+        slowedDown = False # will cause a fire if done before cooldown
+        fireSuppressed_1 = False # will have no effect if there are no fire
+        warnedOfLoops = False #unlocks with the INFO option, enables some alternate text.
+        depolorized = False #unlocks with the reversed option, it adds more time loops.
+
+        fixing_reactor_loop = True
+
+        while fixing_reactor_loop:
+            ind, val = game.choose2(choices, "What procedure will you attept?" )
+            if val == "SHUTDOWN":
+                game.rolltext("""
+You followed the instructions to shut down the the tacheon emitter array.
+...
+The system register succesful, then not successful shutdown, then both at the same time.
+                """)
+                if warnedOfLoops:
+                    game.rolltext("""
+It took you a moment to see it, but you now realized why this is.
+Looking over the diagnatic data you retrived earlier,
+you see most of the emitters are stuck in time-loops.
+They are running perpetually.
+Trying to just shutting them down just won't work.
+                    """)
+                else:
+                    game.rolltext("""
+You are completly baffled at what this. You ended the shutdown process.
+You decide to try something else.
+                    """)
+                choices.pop(ind)
+            elif val == "REVERSE":
+                game.rolltext("""
+You followed the instructions to depolarize the tacheon emitters, whatever that means.
+Shortly after doing so, you noticed something quite off.
+A hole just appeared in the side of the wall, then seemed to reassamle into a wall again before suddenly turning back into a hole, and repeated.
+Whatever you just did, you are pretty sure you just made it all worse.
+You decided not to try that again.
+                """)
+                depolorized = True
+                choices.pop(ind)
+            elif val == "INSTADEATH":
+                nav.running = False
+                fixing_reactor_loop = False
+                game.setGameover(game, "You found a way to blow youself up. You are very VERY dead!")
+                game.rolltext("""
+You followed the instructions for initiating an emergency overload.
+Right after pressing the confirm key on the screen, you got an odd feeling.
+The reactor started humming. Did you just make a huge mis..
+...
+You never got to finish that thought. You just ceased to exist.
+To say you were atomized is not entierly accurate,
+but that may be the closest terminology to describe what happened to you.
+At least, there were no pain.
+                """)
+                return
+            elif val == "FIRE SUP 2":
+                game.rolltext("""
+You followed the quite simple instruction, and...
+the entire room just got sprayed by a dense foam, convering every surface.
+yuck.
+If there acually was a fire, you are convinved the fire would be gone by now.
+But there was no fire. 'Why did I do this?' you think to yourself while trying to get foam out of your hair.
+                """)
+                choices.pop(ind)
+            elif val == "FIRE SUP 1":
+                if slowedDown:
+                    game.rolltext("""
+You followed the quite simple instruction,
+The fire in the accellerator ring went out.
+Unfortunatly this also made the accellerater accellerate back up.
+                """)
+                else:
+                    slowedDown = False
+                    game.rolltext("""
+You followed the quite simple instruction,
+Nothing seems to have happened.
+                """)
+            elif val == "HURRY UP":
+                nav.running = False
+                fixing_reactor_loop = False
+                game.setGameover(save, "You got yourself trapped in a timeloop")
+                game.rolltext("""
+The instructions for accellerating the accellerator were quite easy to follow.
+you just adjusted that sqiggly thing, increased that varible,
+pressed that confirm button, changed that dial, adjusted that sqiggly thing,
+increased that varia.. wait.. wha
+you adjusted that sqiggly thing, incr..
+you just had a sudden feeling of déjà vu, as you again adjusted that sqiggly thing.
+
+uh no, you realize, as you got your thinking stright again.
+you try to undo what you did when.. you followed instructions to adjust that sqiggly thi..
+
+'shit' you managed to think as you suddenly find yourself adjusting that squigg..
+and again.. at shorter and shorter intervals.
+
+The after looping for what seemed like forever, with the intervals going shorter and shorter, they suddenly stopped getting shorter.
+From what you could tell, with the little self awareness you could muster, the intervals were at about 1 secund.
+And you soon realied why.
+
+Aside from your little looping bobble, everything was gone.
+You aren't dead. Not really. But by now, you wish you were.
+                """)
+                return
+            elif val == "SLOW DOWN":
+                if slowedDown:
+                    game.rolltext("""
+As you were about to start following the instructions,
+you realized you have already done this. There is no need to do it again.
+                    """)
+                elif not cooledDown:
+                    game.rolltext("""
+You followed the instructions to decelerate the accellerator.
+...
+It seems to work, for a while. Then suddenly a new alarm starts bleeping.
+Fire. There is a fire in the accellerator now.
+                    """)
+                    
+                else:
+                    game.rolltext("""
+You typed in the final instructions for decellerating the system.
+...
+You check the readouts, and several alarms stops beeping.
+The reactor is no longer critical, but that might change.
+                    """)
+                    fixing_reactor_loop = False #Fixed the reactor
+                slowedDown = True
+            elif val == "COOL DOWN":
+                if cooledDown:
+                    game.rolltext("""
+As you were about to start following the instructions,
+you realized you have already done this. There is no need to do it again.
+                    """)
+                elif slowedDown:
+                    game.rolltext("""
+You attempt to follow the instructions for initiating emergency cooling,
+unfortunalty, it seems the fire is making it more or less impossible to initiate the cooling system.
+                    """)
+                else:
+                    game.rolltext("""
+You follow the instructions, and soon the emergency cooling brought the temperature down.
+some of the alarms stops bleeping, and you notice the previusly looping instruction manual disappeard.
+Well, your fingers somewhy still remained there, looping as creepy as before, but now they were reaching for nothing.
+                    """)
+                    cooledDown = True
+            elif val == "INFO":
+                game.rolltext("""
+                ((PLEACEHOLDER))
+                """) #Lots TODO here...
+                warnedOfLoops = True
+        
 
 
     def goto(room):
