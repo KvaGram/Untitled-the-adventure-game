10296590243ad003384cbd58e66fa5da35ee7e1c
Lars Erik Grambo <0kvakk@gmail.com>
2019-08-28 13:10:26 +0200

    cleanup

--- scrollFrame.py
+++ scrollFrame.py
@@ -43,7 +43,7 @@ class ResizingCanvas(TK.Canvas):
         # resize the canvas 
         self.config(width=self.width, height=self.height)
         # rescale all the objects tagged with the "all" tag
-        #self.scale("all",0,0,wscale,hscale)
+        self.scale("all",0,0,wscale,hscale)
 
 
 # ********************************
