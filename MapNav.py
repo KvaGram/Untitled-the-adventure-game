362614c553df8d14d2ec49c913b7931a36c0c97e
Lars Erik Grambo <0kvakk@gmail.com>
2019-09-11 11:01:12 +0200

    lunsj-stash

--- MapNav.py
+++ MapNav.py
@@ -1,36 +1,146 @@
-from untitled_const import IMAGE_DIR
 import os
 import tkinter as TK
 from PIL import Image, ImageTk
+import math
+import time
 
-_TEST_BACK_IMG = os.path.join(IMAGE_DIR, "mapB.png")
-_TEST_MID_IMG  = os.path.join(IMAGE_DIR, "InnerB.png")
-_TEST_FRONT_IMG = os.path.join(IMAGE_DIR, "dot.png")
-_MAP_SIZE = (200, 200)
+from untitled_const import(
+IMAGE_DIR,
+DEFALT_BACK_IMG,
+DEFAULT_LIT_IMG,
+DEFAULT_DOT_IMG,
+UNKNOWN_IMG,
+DEFAULT_MAP_SIZE,
+DEFAULT_DOT_SIZE,
+TAU,
+) #constants imported.
 
 class Mapnav(TK.Frame):
     def __init__(self, master, **kw):
         super().__init__(master = master, **kw)
+        self._dotstate = None
+        self._dotimage =  DEFAULT_DOT_IMG       
+        self._backimage =  DEFALT_BACK_IMG       
+        self._litimage =  DEFAULT_LIT_IMG       
 
-        backlayerData  = Image.open(_TEST_BACK_IMG ).convert("RGBA").resize(_MAP_SIZE)
-        midlayerData   = Image.open(_TEST_MID_IMG  ).convert("RGBA").resize(_MAP_SIZE)
-        frontlayerData = Image.open(_TEST_FRONT_IMG).convert("RGBA")
+        self.image = None   #Image.new("RGBA", _MAP_SIZE, "white")
+        self.tkImage = None #ImageTk.PhotoImage(image=self.image)
 
-        combo = Image.new("RGBA", _MAP_SIZE, "white")
-        combo.paste(backlayerData, (0,0), backlayerData)
-        combo.paste(midlayerData, (0,0), midlayerData)
-        combo.paste(frontlayerData, (40,120), frontlayerData)
-
-        self.imagedata = ImageTk.PhotoImage(image=combo)
-        self.mapImage = TK.Label(master= self, image = self.imagedata)
+        self.mapImage = TK.Label(master = self)
         self.nameLabel  = TK.Label(master = self, text = "Example")
+
+        self.refresh()
 
         self.mapImage.pack()
         self.nameLabel.pack()
+    def refresh(self):
+        self.image = Image.new("RGBA", DEFAULT_MAP_SIZE, "white")
+
+        back = Image.open(self._backimage).convert("RGBA").resize(DEFAULT_MAP_SIZE)
+        self.image.paste(back, (0,0), back)
+
+        if (self._backimage):
+            high = Image.open(self._backimage).convert("RGBA").resize(DEFAULT_MAP_SIZE)    
+            self.image.paste(high, (0,0), high)
+        if (self._dotstate):
+            assert(type(self._dotstate) is tuple and len(self._dotstate) == 2)
+            dot = Image.open(self._dotimage).convert("RGBA").resize(DEFAULT_DOT_SIZE)
+            d0 = self._dotstate[0] - DEFAULT_DOT_SIZE[0]/2 + DEFAULT_MAP_SIZE[0]/2
+            d1 = self._dotstate[1] - DEFAULT_DOT_SIZE[1]/2 + DEFAULT_MAP_SIZE[1]/2
+            self.image.paste(dot, (math.floor(d0),math.floor(d1)), dot)
+
+        self.tkImage = ImageTk.PhotoImage(image=self.image)
+        self.mapImage.config(image = self.tkImage)
+    def PlaceDot(radian, radius):
+        x  = math.floor(math.cos(rot) * rad)
+        y  = math.floor(math.sin(rot) * rad)
+    #region setters and getters
+    @property
+    def DotState(self):
+        return self._dotstate
+    @DotState.setter
+    def DotState(self, val):
+        self._dotstate = val
+        self.refresh()
+    @property
+    def DotImage(self):
+        return self._dotimage
+    @DotImage.setter
+    def DotImage(self, val):
+        _dotimage = val
+        self.refresh()
+    @property
+    def BackImage(self):
+        return self._backimage
+    @BackImage.setter
+    def BackImage(self, val):
+        _backimage = val
+        self.refresh()
+    @property
+    def LitImage(self):
+        return self._litimage
+    @LitImage.setter
+    def LitImage(self, val):
+        _litimage = val
+        self.refresh()
+    #endregion setters and getters
+    
+
+#imported from C# mathf library
+def Repeat(t:float, length:float) -> float:
+    return t - math.floor(t/length) * length
+
+def pingpong(t, length) -> float:
+    t = Repeat(t, length * 2)
+    return length - abs(t - length)
+
 
 if __name__ == "__main__":
     root = TK.Tk()
     mapnav = Mapnav(master = root)
-    
     mapnav.pack()
-    root.mainloop()
+
+    maxRad = 100
+    radSpeed = 0.5 * 100
+
+    rotspeed = 0.1 * TAU
+
+    rad = 0
+    rot = 0
+
+    startTime = time.time()
+    doAnimate = TK.BooleanVar(value = True)
+    TK.Checkbutton(text = "Animate", variable = doAnimate).pack()
+    
+    radVar = TK.StringVar(value = rad)
+    rotVar = TK.StringVar(value = rot)
+    TK.Label(master=root, text = "Radius").pack()
+    TK.Entry(master=root, textvariable = radVar).pack()
+    TK.Label(master=root, text = "Rotation").pack()
+    TK.Entry(master=root, textvariable = rotVar).pack()
+
+    info = TK.Label(master = root)
+    info.pack()
+    while(True):
+        if(doAnimate.get()):
+            t = time.time() - startTime
+            rad = pingpong(t * radSpeed, maxRad)
+            rot = Repeat(t * rotspeed, TAU)
+
+            radVar.set(f"{rad:.2f}")
+            rotVar.set(f"{rot:.2f}")
+        else:
+            try:
+                rad = float(radVar.get())
+            except ValueError:
+                rad = 0
+            try:
+                rot = float(rotVar.get())
+            except ValueError:
+                rot = 0        
+        x  = math.floor(math.cos(rot) * rad)
+        y  = math.floor(math.sin(rot) * rad)
+        mapnav.DotState = (x, y)
+        info.config(text = f"X {x}, Y {y}")
+        root.update()
+        root.update_idletasks()
--- untitled_const.py
+++ untitled_const.py
@@ -1,5 +1,7 @@
 #constants for Untitled! The adventure game and text editor
 import sys
+import os
+import math
 
 ENTRY_NAME_PATTERN = r'^[A-Z]+(?:[0-9]|_|[A-Z])*$'
 ITEM_NAME_PATTERN = r'^[A-Z]+(?:[0-9]|_|[A-Z])*$'
@@ -21,6 +23,15 @@ DATATERM_ITEM = "ITEM"
 DATATERM_NAME = "_NAME"
 DATATERM_DESCRIPTION = "_DESC"
 DATATERM_ICON = "_ICON"
+
+DEFALT_BACK_IMG = os.path.join(IMAGE_DIR, "mapB.png")
+DEFAULT_LIT_IMG  = os.path.join(IMAGE_DIR, "InnerB.png")
+DEFAULT_DOT_IMG = os.path.join(IMAGE_DIR, "dot.png")
+UNKNOWN_IMG = os.path.join(IMAGE_DIR, "mapc.png")
+DEFAULT_MAP_SIZE = (200, 200)
+DEFAULT_DOT_SIZE = (20, 20)
+
+TAU = math.pi * 2
 
 class ReturnToMain(Exception):
     def __init__(self, *args, **kwargs):
